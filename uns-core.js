b2fb181cf578d5f2e93f02684c1aad0ca9b8fc4d
Mithgol the Webmaster <getgit@mithgol.ru>
2016-01-10 23:53:23 +0300

    separate the core from the CLI, add CLI help and JSHint-based testing

---
+++ uns-core.js
@@ -0,0 +1,5 @@
+var clog = console.log;
+
+module.exports = filenameHPT => {
+   clog(`Reading area descriptions from ${filenameHPT}...`);
+};
--- unsquish.js
+++ unsquish.js
@@ -1,3 +1,52 @@
 #!/usr/bin/env node
 
-console.log('Hello world.');
+var unsquish = require('./');
+var clog = console.log;
+
+var params = [].concat(process.argv);
+params.shift(); // 'node'
+params.shift(); // 'unsquish'
+
+var rusMode = false;
+params = params.filter(nextParam => {
+   if( nextParam.toLowerCase() === '--rus' ){
+      rusMode = true;
+      return false;
+   }
+
+   return true;
+});
+
+if( params.length < 1 ){
+   clog('');
+   if( rusMode ){
+      clog('Использование:');
+      clog('   unsquish configHPT');
+      clog('');
+      clog('Параметры:');
+      clog('');
+      clog('configHPT -- путь к файлу конфигурации областей эхопочты HPT.');
+      clog('');
+      clog('Необязательный параметр "--rus" предписывает приложению');
+      clog('употребление русских сообщений вместо английских.');
+      clog('');
+      clog('(Воздействует только на вывод в консоли, не на выходные файлы.)');
+   } else {
+      clog('Usage:');
+      clog('   unsquish configHPT');
+      clog('');
+      clog('Parameters:');
+      clog('');
+      clog('configHPT -- path to the HPT echomail area configuration file.');
+      clog('');
+      clog('An optional "--rus" parameter dictates the application to use');
+      clog('Russian messages instead of English.');
+      clog('');
+      clog('(It only affects console output, not the output files.)');
+   }
+   process.exit(1);
+}
+
+var filenameHPT = params.pop();
+
+unsquish(filenameHPT);
