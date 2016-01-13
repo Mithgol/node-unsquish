2d680cf9e2b3ec44d09559d7bb83abbea2978b47
Mithgol the Webmaster <getgit@mithgol.ru>
2016-01-13 22:27:48 +0300

    read echomail area names and paths from the fidoconfig file

--- uns-core.js
+++ uns-core.js
@@ -7,4 +7,15 @@ var clog = console.log;
 module.exports = filenameHPT => {
    cl.status(`Operating on area descriptions from ${filenameHPT}`);
    var echoconf = fidoconfig.areas(filenameHPT);
+   var areaPaths = echoconf.getAreaNames().reduce((prevArr, nextName) => {
+      var nextArea = echoconf[nextName];
+      if( nextArea.passthrough ){
+         cl.skip(`Area ${nextArea.configName} is passthough. Skipped.`);
+         return prevArr;
+      }
+      return prevArr.concat({
+         areaName: nextArea.configName,
+         areaPath: nextArea.path
+      });
+   }, []);
 };
